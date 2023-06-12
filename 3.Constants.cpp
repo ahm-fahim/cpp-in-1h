a6524b4d8a90618835868b3692d7e89fe8332630
ahm-fahim <contactahmfahim@gmail.com>
2023-06-12 13:32:03 +0600

    constant and namming convention

---
+++ 3.Constants.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const double pi = 3.14;
+    // pi = 3;  // constant variable not allow redeclare value
+
+    return 0;
+}
---
+++ 4.NammingConventions.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int file_size; // Snake Case
+    int FileSize;  // Pascal Case
+    int fileSize;  // Camel Case
+    int iFileSize; // Hungarian Notation
+}
