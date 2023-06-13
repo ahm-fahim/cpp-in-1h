e3e0c5daa20615e8cee9ee58de08655287bd80fe
ahm-fahim <contactahmfahim@gmail.com>
2023-06-13 07:20:41 +0600

    innitialize num

---
+++ 8.InnitializeVariables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int var1 = 34;
+    double var2 = 432.2;
+    char var3 = 'f';
+    long var4 = 454644L;
+    float var5 = 343.134F;
+    bool var6 = false;
+    auto var7 = 43546L;
+    int var8{};
+
+    
+
+}
